ea7ddb9f5dbc38cc534c932104ea83d1cc8f919f
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2023-03-13 22:13:35 +0600

    useLocation Hook in React

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  let location = useLocation();
+
+  useEffect(() => {
+    // console.log(location.pathname);
+  }, [location]);
+
   return (
     <>
       <nav
@@ -10,7 +16,7 @@ const Navbar = () => {
       >
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            iNotes
+            iNotebook
           </Link>
           <button
             className="navbar-toggler"
@@ -26,12 +32,23 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                  aria-current="page"
+                  to="/"
+                >
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about/">
+                <Link
+                  className={`nav-link ${
+                    location.pathname === "/about" ? "active" : ""
+                  }`}
+                  to="/about/"
+                >
                   About
                 </Link>
               </li>
--- client/src/context/notes/NoteState.jsx
+++ client/src/context/notes/NoteState.jsx
@@ -1,27 +1,8 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Asad",
-    class: "5d",
-  };
-
-  const [state, setState] = useState(s1);
-
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Akul",
-        class: "10b",
-      });
-    }, 1000);
-  };
-
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
